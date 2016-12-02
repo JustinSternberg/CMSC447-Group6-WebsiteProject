217584aeebfb934a17ba8d40ae3c02fab4d3054c
linkvetern64 <jstand1@umbc.edu>
2016-12-02 00:46:58 -0500

    User profiles
    Added user profile template.

--- webapp/view/home.php
+++ webapp/view/home.php
@@ -32,6 +32,8 @@ if(!$_SESSION["auth"]){
     <!-- Theme CSS -->
     <link href="css/freelancer.min.css" rel="stylesheet">
     <link href="css/styles.css" type="text/css" rel="stylesheet">
+    <link href="css/profile.css" type="text/css" rel="stylesheet">
+
     <!-- Custom Fonts -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
@@ -71,7 +73,7 @@ if(!$_SESSION["auth"]){
 
 </head>
 
-<body id="page-top" class="index">
+<body id="page-top" class="index" bgcolor="#">
 
 <!-- Navigation -->
 <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
@@ -115,17 +117,85 @@ if(!$_SESSION["auth"]){
 
 <!-- Header -->
 <header>
-    <div class="container">
+    Welcome back <?php echo $_SESSION["name"]; ?>
+    <!-- <div class="container">
         <div id="profile-options">
             <div id="profile-picture">
                 <img src="img/profiles/1.jpg" class="fit"/>
             </div>
-            Welcome back <?php echo $_SESSION["name"]; ?>
         </div>
         <div id="listings">
             open content
         </div>
+    </div> -->
+    <!--
+User Profile Sidebar by @keenthemes
+A component of Metronic Theme - #1 Selling Bootstrap 3 Admin Theme in Themeforest: http://j.mp/metronictheme
+Licensed under MIT
+-->
+
+    <div class="container">
+        <div class="row profile">
+            <div class="col-md-3">
+                <div class="profile-sidebar">
+                    <!-- SIDEBAR USERPIC -->
+                    <div class="profile-userpic">
+                        <img src="http://keenthemes.com/preview/metronic/theme/assets/admin/pages/media/profile/profile_user.jpg" class="img-responsive" alt="">
+                    </div>
+                    <!-- END SIDEBAR USERPIC -->
+                    <!-- SIDEBAR USER TITLE -->
+                    <div class="profile-usertitle">
+                        <div class="profile-usertitle-name">
+                            Marcus Doe
+                        </div>
+                        <div class="profile-usertitle-job">
+                            Developer
+                        </div>
+                    </div>
+                    <!-- END SIDEBAR USER TITLE -->
+                    <!-- SIDEBAR BUTTONS -->
+                    <div class="profile-userbuttons">
+                        <button type="button" class="btn btn-success btn-sm">Follow</button>
+                        <button type="button" class="btn btn-danger btn-sm">Message</button>
+                    </div>
+                    <!-- END SIDEBAR BUTTONS -->
+                    <!-- SIDEBAR MENU -->
+                    <div class="profile-usermenu">
+                        <ul class="nav">
+                            <li class="active">
+                                <a href="#">
+                                    <i class="glyphicon glyphicon-home"></i>
+                                    Overview </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="glyphicon glyphicon-user"></i>
+                                    Account Settings </a>
+                            </li>
+                            <li>
+                                <a href="#" target="_blank">
+                                    <i class="glyphicon glyphicon-ok"></i>
+                                    Tasks </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="glyphicon glyphicon-flag"></i>
+                                    Help </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- END MENU -->
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="profile-content">
+                    Some user related content goes here...
+                </div>
+            </div>
+        </div>
     </div>
+    <br>
+    <br>
 </header>
 
 <!-- Plugin JavaScript -->
